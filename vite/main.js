7d063ef8a2d385b69aec968dff921ccde2f62351
Arush Singh <sarushmad@gmail.com>
2024-09-09 10:11:30 +0530

    3D demo

--- vite/main.js
+++ vite/main.js
@@ -1,23 +1,29 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById("canvas");
 
 // 1 CreateScene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color('#F0F0F0');  
+scene.background = new THREE.Color("#F0F0F0");
 
 // Add camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); 
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 camera.position.z = 5;
 
 // Add object
 const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshBasicMaterial({ color: '#468585' });
+const material = new THREE.MeshLambertMaterial({ color: "#468585", emissive: "#468585" });
 const dodecahedron = new THREE.Mesh(geometry, material);
 
 const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
-const boxMaterial = new THREE.MeshLambertMaterial({ color: '#B4B4B3' });
-const box = new THREE.Mesh(boxGeometry, material);
+const boxMaterial = new THREE.MeshLambertMaterial({ color: "#B4B4B3", emissive: "B4B4B3" });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
 box.position.y = -1.5;
 
 scene.add(dodecahedron);
@@ -29,6 +35,29 @@ light.position.set(1, 1, 1);
 scene.add(light);
 
 // add rendered
-const rendered = new THREE.WebGLRenderer({ canvas });
-rendered.setSize(window.innerWidth, window.innerHeight);
-rendered.render(scene, camera);
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.render(scene, camera);
+
+// add OrbitControl
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+// controls.dampingFactor = 0.05;
+// controls.enableZoom = true;
+// controls.enablePan = true;
+
+// //  Add animations
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   dodecahedron.rotation.x += 0.01;
+//   dodecahedron.rotation.y += 0.01;
+
+//   box.ratation.y += 0.005;
+
+//   controls.update();
+  
+// }
+
+// animate();
