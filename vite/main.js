e3ed23b7b4f905f3bedb43a3f86607785de4a442
Arush Singh <sarushmad@gmail.com>
2024-09-10 12:38:13 +0530

    window resized

--- vite/main.js
+++ vite/main.js
@@ -60,4 +60,15 @@ renderer.render(scene, camera);
   
 // }
 
+// Handle window resizing
+
+// window.addEventListener('resize', () => {
+//   camera.aspect = window.innerWidth / window.innerHeight; 
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+// });
+
+
 // animate();
+
+
